2e7e9ea0f0d7e04628c262f2512cd67f54ff1010
SheriffCarry-backup <intsheriff@gmail.com>
2025-02-04 08:09:18 +0100

    Create OpenAllCards_live_updating.js

---
+++ ConsoleScripts/OpenAllCards_live_updating.js
@@ -0,0 +1,8 @@
+async function start_cards() {
+  let response = await fetch(
+    "https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/ConsoleScripts/Open%20All%20Cards.js"
+  );
+  let text = await response.text();
+  eval(text);
+}
+start_cards()
